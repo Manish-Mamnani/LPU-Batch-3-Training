54c613c342dd9fe8068aa55c529e17a38ef26b97
Manish Mamnani <mkm30882@gmail.com>
2025-12-27 09:42:25 +0530

    Day 7

---
+++ Practice/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string s = Console.ReadLine();
+
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            foreach(char c in s)
+            {
+                if (frequency.ContainsKey(c))
+                {
+                    frequency[c]++;
+                }
+                else
+                {
+                    frequency[c] = 1;   
+                }
+            }
+
+            foreach(var i in frequency)
+            {
+                Console.WriteLine($"Frequency of {i.Key}: {i.Value}");
+            }
+
+            List<int> arr = new List<int>();
+
+            arr.Add(Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"{arr[0]}");
+
+
+        }
+    }
+}
